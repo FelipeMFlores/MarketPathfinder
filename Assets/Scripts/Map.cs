9d8df862d9209b67e22fc5e83667623499527ec2
FelipeMFlores <felipe.melloflores@gmail.com>
2019-05-28 19:54:59 -0300

    Add map

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using UnityEngine.SceneManagement;
+
+public class Map : MonoBehaviour
+{
+
+    public GameObject position0;
+    public Button start;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartPress()
+    {
+        position0.SetActive(false);
+    }
+
+}
