0be37edc3df783ead5a21d118c917ef0ecfadc96
FelipeMFlores <felipe.melloflores@gmail.com>
2019-05-28 21:20:25 -0300

    Add Product Location and method to turn on Locations

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 using UnityEngine.UI;
 
 using UnityEngine.SceneManagement;
+using System;
 
 public class Map : MonoBehaviour
 {
 
-    public GameObject position0;
-    public Button start;
+    public GameObject[] locations = new GameObject[25];
+    public Button redo;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+
     }
 
     // Update is called once per frame
@@ -22,9 +25,25 @@ public class Map : MonoBehaviour
     {
         
     }
-    public void StartPress()
-    {
-        position0.SetActive(false);
+
+    void TurnLocations(bool[] locationsStatus){
+        for (int i = 0; i < locationsStatus.Length; i++)
+        {
+            locations[i].SetActive(locationsStatus[i]);
+        }
+
     }
+    
+    public void RedoPress(){
+        System.Random rand = new System.Random();
+        bool[] locationsStatus = new bool[25];
+        for (int i = 0; i < locationsStatus.Length; i++)
+        {
+            locationsStatus[i] = rand.Next(2) == 0;
+        }
+        
+        TurnLocations(locationsStatus);
+    }
+
 
 }
